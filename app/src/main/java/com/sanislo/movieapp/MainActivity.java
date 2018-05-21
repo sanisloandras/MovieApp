2dc77d11881fe9a5e53866aef39ffe70800ced4a
sanisloandras <sanisloandras@gmail.com>
2018-05-21 15:26:38 +0300

    Initial commit

---
+++ app/src/main/java/com/sanislo/movieapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sanislo.movieapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
