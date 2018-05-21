b1792100bebe2a91f56466808d5cac8977130191
sanisloandras <sanisloandras@gmail.com>
2018-05-21 18:43:03 +0300

    basic movie details layout

---
+++ app/src/main/java/com/sanislo/movieapp/persistence/api/MovieAppApi.java
@@ -0,0 +1,12 @@
+package com.sanislo.movieapp.persistence.api;
+
+import com.sanislo.movieapp.persistence.response.MovieResponse;
+
+import java.util.List;
+
+import io.reactivex.Single;
+import retrofit2.http.GET;
+
+public interface MovieAppApi {
+
+}
--- app/src/main/java/com/sanislo/movieapp/presentation/MainActivity.java
+++ app/src/main/java/com/sanislo/movieapp/presentation/MainActivity.java
@@ -1,7 +1,9 @@
-package com.sanislo.movieapp;
+package com.sanislo.movieapp.presentation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.sanislo.movieapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
