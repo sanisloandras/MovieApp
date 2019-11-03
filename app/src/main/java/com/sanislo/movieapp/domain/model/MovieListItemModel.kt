ce6ad5a44235355c744dac544f93578c333ad1f0
blb-szaniszlo <Andras.Szaniszlo@wup.hu>
2019-11-03 10:57:49 +0100

    refreshing project

---
+++ app/src/main/java/com/sanislo/movieapp/domain/model/MovieListItemModel.kt
@@ -0,0 +1,3 @@
+package com.sanislo.movieapp.domain.model
+
+data class MovieListItemModel(var _id: Int, var overview: String?, var originalLanguage: String?, var originalTitle: String?, var isVideo: Boolean, var title: String?, var posterPath: String?, var backdropPath: String?, var releaseDate: String?, var popularity: Double, var voteAverage: Double, var id: Int, var isAdult: Boolean, var voteCount: Int)
